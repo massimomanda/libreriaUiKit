405cc3c6307da78d7dea4ff51d611416ef524195
Ilaria <idellaquila@sysmanagement.it>
2022-07-05 17:42:22 +0200

    chiamata agli user

--- projects/testing/src/app/app.component.ts
+++ projects/testing/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { BehaviorSubject, Observable, Observer, Subject } from 'rxjs';
 
 import { ToastService } from './services/toast.service';
@@ -10,8 +12,13 @@ import { ToastService } from './services/toast.service';
 })
 export class AppComponent implements OnInit {
   title = 'testing';
-  text!:string
+  text!: string
   pippo: EventEmitter<any> = new EventEmitter();
+  formAutocomplete: FormGroup = {} as FormGroup;
+  users: any = [];
+  searchResult: any = [];
+  messaggio!: string;
+
   // pluto: Subject<any> = new Subject()
   // pippo = new Observable(subscriber => {
   //   subscriber.next(1);
@@ -22,11 +29,14 @@ export class AppComponent implements OnInit {
   //     subscriber.complete();
   //   }, 1000);
   // });
-  constructor(public toast:ToastService){}
+  constructor(public toast: ToastService, private _fb: FormBuilder, private http: HttpClient) { }
 
-  ngOnInit(){
+  ngOnInit() {
+    this.formAutocomplete = this._fb.group({
+      autocomplete: [''],
 
-
+    });
+    this.getAllUsers()
     this.toast.setMessage('ciao')
     //this.pippo.emit('vafammocc')
     // this.pippo.subscribe({
@@ -34,10 +44,50 @@ export class AppComponent implements OnInit {
     //   error(err) { console.error('something wrong occurred: ' + err); },
     //   complete() { console.log('done'); }
     // });
-    
+    this.formAutocomplete.valueChanges.subscribe((res: any) => {
+      let risposta = res.autocomplete
+
+      // if(this.formAutocomplete.value.autocomplete !== '' && this.searchResult == []){
+
+      //  this.searchResult = []
+      //  this.messaggio = 'Nessun risultato'
+
+      // }
+      if (risposta.length !== 0) {
+
+        this.searchResult = this.users.filter((r: any) => r.toLowerCase().includes(risposta)).slice(0, 5)
+
+        console.log(this.searchResult)
+        if(this.searchResult === []){
+          console.log('ciao')
+        }
+        
+      } else{
+        this.searchResult = []
+      }
+    })
   }
 
-  metodo(){
+  metodo() {
     console.log('ciao')
+  }
+  get contenutoBacheca() {
+    return this.formAutocomplete?.get('autocomplete');
+  }
+
+  getInputValue() {
+    // console.log(this.formAutocomplete.value.autocomplete)
+  }
+
+  getAllUsers() {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((res: any) => {
+      let users = res;
+      // console.log(users)
+      users.forEach((user: any) => {
+        // console.log(user.name)
+        this.users.push(user.name)
+      })
+      console.log(this.users)
+    })
   }
 }
